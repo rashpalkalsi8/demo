25496b4966076414c3ec839defad6ea5153c6d97
rashpalkalsi8 <rashpalkalsi8@gmail.com>
2023-03-29 16:41:16 +0530

    new

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
+  formatLabel(value: number): string {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
 
+    return `${value}`;
+  }
 }
