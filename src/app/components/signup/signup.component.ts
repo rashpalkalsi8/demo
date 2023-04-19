55979ee1e5ebd7f332e25558f762fdb7937bba27
rashpalkalsi8 <rashpalkalsi8@gmail.com>
2023-04-19 10:41:27 +0530

    First

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl,FormGroup,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  constructor() { }
 
+  ngOnInit(): void {
+  }
+
+  signupForm = new FormGroup({
+    fname: new FormControl('',Validators.required),
+    lname: new FormControl(''),
+    usr: new FormControl('',Validators.required),
+    email: new FormControl('',Validators.required),
+    pwd: new FormControl('',Validators.required)
+  })
+
+  get fname(){return this.signupForm.get ('fname')}
+  get email(){return this.signupForm.get ('email')}
+  get usr(){return this.signupForm.get ('usr')}
+  get pwd(){return this.signupForm.get ('pwd')}
+
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = " fa-eye-slash";
+
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
 }
