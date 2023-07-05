f84c63de4eae8d855191670ff7edf6441ea9b86a
rashpalkalsi8 <rashpalkalsi8@gmail.com>
2023-07-05 12:40:22 +0530

    New

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl,FormGroup,Validators,FormBuilder} from '@angular/forms';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
--- src/app/services/user-data.service.ts
+++ src/app/services/user-data.service.ts
@@ -8,7 +8,7 @@ export class UserDataService {
 
   header = new HttpHeaders({
     'x-api-host': 'digitalstories.co.in',
-    'x-api-key': ''
+    'x-api-key': 'f094fdf9-5718-4858-aa72-64136530c582'
   });
   url = 'https://digitalstories.co.in/api/v1/customers/signup';
   constructor(private http: HttpClient) { }
