838bb1de50d21ec9010c1abd3afb84406a3122e8
rashpalkalsi8 <rashpalkalsi8@gmail.com>
2023-08-04 17:19:00 +0530

    New

--- src/app/components/book-a-session/book-a-session.component.ts
+++ src/app/components/book-a-session/book-a-session.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-a-session',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-a-session.component.css']
 })
 export class BookASessionComponent {
+
+  public mname: any[] = [];
+
+  constructor(private router: Router, private httpclient: HttpClient) {
+    
+  }
+
+  ngOnInit() {
+  this.dologin1()
+  }
+
+
+  dologin1() {
+    let api = 'f094fdf9-5718-4858-aa72-64136530c582';
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-api-key': api
+    });
+    let url = "https://digitalstories.co.in/api/v1/mentors"
+    this.httpclient.get<any>(url, { headers })
+      .subscribe((res: any) => {
+        console.log(res);
+        this.mname = res.name;
+        return this.mname = res;
+      })
+  }
 
 }
